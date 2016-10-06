1e8b78819cbff4f056e41f36e2689893780e33e3
Jamc <jiayu.ji@gmail.com>
2016-10-06 22:34:58 +0800

    Update: removed endpoints from application class

--- src/main/java/me/jamc/skeleton/Application.java
+++ src/main/java/me/jamc/skeleton/Application.java
@@ -1,34 +1,14 @@
 package me.jamc.skeleton;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import me.jamc.skeleton.config.DatabaseConfig;
 
 @RestController
 @SpringBootApplication
 @ServletComponentScan
 public class Application {
-
-    @Autowired
-    private GreetingBean greeting;
-
-    @Autowired
-    private DatabaseConfig config;
-
-    @RequestMapping("/")
-    String home() {
-        return greeting.getWords();
-    }
-
-    @RequestMapping("/database")
-    String database() {
-        return config.getUrl() + " " + config.getUsername() + " " + config.getPassword();
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
