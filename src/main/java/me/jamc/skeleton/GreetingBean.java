86c24b5b70c71e568a1fd603308813fae62dd374
Jamc <jiayu.ji@gmail.com>
2016-10-06 12:58:42 +0800

    Update: Added validation on bean properties

--- src/main/java/me/jamc/skeleton/GreetingBean.java
+++ src/main/java/me/jamc/skeleton/GreetingBean.java
@@ -1,11 +1,13 @@
 package me.jamc.skeleton;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GreetingBean {
 
+    @NotEmpty
     @Value("${web.skeleton.greeting}")
     private String words;
 
