f865c60ad153f59f0bb2f6b5c4802e0cd73064af
Jamc <jiayu.ji@gmail.com>
2016-10-07 14:36:12 +0800

    Update: removed useless annotation

--- src/main/java/me/jamc/skeleton/Application.java
+++ src/main/java/me/jamc/skeleton/Application.java
@@ -3,9 +3,7 @@ package me.jamc.skeleton;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 @ServletComponentScan
 public class Application {
@@ -13,4 +11,5 @@ public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+
 }
