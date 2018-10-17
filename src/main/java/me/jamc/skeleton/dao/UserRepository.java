dce121869130242261c65b3d67cc87f05a38aa86
Jiayu <jiayu@JiayudeMacBook-Pro-2.local>
2018-10-17 11:52:31 +0800

    a simplified version for testing on cloud:

--- src/main/java/me/jamc/skeleton/dao/UserRepository.java
+++ src/main/java/me/jamc/skeleton/dao/UserRepository.java
@@ -6,5 +6,4 @@ import me.jamc.skeleton.model.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
-    User findByUsername(String username);
 }
--- src/main/java/me/jamc/skeleton/service/UserServiceImpl.java
+++ src/main/java/me/jamc/skeleton/service/UserServiceImpl.java
@@ -3,11 +3,6 @@ package me.jamc.skeleton.service;
 import com.google.common.collect.ImmutableList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -18,7 +13,7 @@ import me.jamc.skeleton.dao.UserRepository;
 import me.jamc.skeleton.model.User;
 
 @Service
-public class UserServiceImpl implements UserService, UserDetailsService{
+public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepo;
@@ -64,48 +59,5 @@ public class UserServiceImpl implements UserService, UserDetailsService{
         List<User> list = new LinkedList<User>();
         userRepo.findAll().forEach( u -> list.add(u));
         return list;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepo.findByUsername(username);
-
-        return new UserDetails() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-
-                return ImmutableList.of(new SimpleGrantedAuthority(user.getRole()));
-            }
-
-            @Override
-            public String getPassword() {
-                return user.getPassword();
-            }
-
-            @Override
-            public String getUsername() {
-                return user.getUsername();
-            }
-
-            @Override
-            public boolean isAccountNonExpired() {
-                return true;
-            }
-
-            @Override
-            public boolean isAccountNonLocked() {
-                return true;
-            }
-
-            @Override
-            public boolean isCredentialsNonExpired() {
-                return true;
-            }
-
-            @Override
-            public boolean isEnabled() {
-                return true;
-            }
-        };
     }
 }
